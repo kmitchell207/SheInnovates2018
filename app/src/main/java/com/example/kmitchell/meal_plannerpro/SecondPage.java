0695b5c781e119c36b6ef0617aba557bdb57c6e4
Kristiana Mitchell <klm207@pitt.edu>
2018-01-27 20:57:27 -0500

    the next next best thing

---
+++ app/src/main/java/com/example/kmitchell/meal_plannerpro/SecondPage.java
@@ -0,0 +1,16 @@
+package com.example.kmitchell.meal_plannerpro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SecondPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_page);
+    }
+
+}
