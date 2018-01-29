21cdab33058ab6dc86bb97753d17e7fbf7d3fcae
APaonessa <amp224@pitt.edu>
2018-01-29 11:47:23 -0500

    Trying to commit

--- app/src/main/java/com/example/kmitchell/meal_plannerpro/MainActivity.java
+++ app/src/main/java/com/example/kmitchell/meal_plannerpro/MainActivity.java
@@ -31,7 +31,7 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-
+//MainActivity
 public class MainActivity extends AppCompatActivity {
 
     Button mButton;
